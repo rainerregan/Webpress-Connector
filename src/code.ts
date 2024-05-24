e0407bfac38b52fbff67372473c0f724a1915dd6
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-05-24 16:52:07 +0700

    Refactor code to improve exporting functionality

--- src/code.ts
+++ src/code.ts
@@ -6,7 +6,10 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'generate-html-css') {
     try {
       const rootNodes = figma.currentPage.selection;
+      console.log("Exported Node", rootNodes);
+      
       if (rootNodes.length === 0) throw new Error('Please select a frame to export.');
+      if (rootNodes.length > 1) throw new Error('Please select a frame one at a time.');
 
       const htmlCss = convertRootNodeToHtmlCss([...rootNodes]);
       figma.ui.postMessage({ type: 'html-css', content: htmlCss });
--- src/generate-styles.ts
+++ src/generate-styles.ts
@@ -1,5 +1,31 @@
 import { getColorString, rgbToHex } from "./color-utils";
 
+function getGeneralStyles(node: SceneNode): string {
+  let styles = '';
+
+  if ('opacity' in node) {
+    styles += `opacity: ${String(node.opacity)}; `;
+  }
+
+  //  If parent is not null, Add absolute position if parent is a frame and is not on layoutMode
+  if (node.parent !== null) {
+    const parent = node.parent as FrameNode;
+    if (parent.type === 'FRAME' && parent.layoutMode === 'NONE') {
+      styles += `position: absolute; `;
+
+      // Add top and left position if parent is not on layoutMode
+      if ('x' in node) {
+        styles += `left: ${String(node.x)}px; `;
+      }
+      if ('y' in node) {
+        styles += `top: ${String(node.y)}px; `;
+      }
+    }
+  }
+
+  return styles;
+}
+
 function getFrameStyles(node: FrameNode): string {
   let styles = '';
 
@@ -7,14 +33,35 @@ function getFrameStyles(node: FrameNode): string {
     const fill = (node.fills as Paint[])[0];
     if (fill.type === 'SOLID') {
       const opacity = fill.opacity !== undefined ? fill.opacity : 1;
-      console.log(rgbToHex(fill.color));
-
       styles += `background-color: ${rgbToHex(fill.color)}; `;
     }
   }
 
   if ('cornerRadius' in node) {
     styles += `border-radius: ${String(node.cornerRadius)}px; `;
+  }
+
+  if ('height' in node) {
+    styles += `height: ${String(node.height)}px; `;
+  }
+
+  if ('width' in node) {
+    styles += `width: ${String(node.width)}px; `;
+  }
+
+  if ('clipsContent' in node) {
+    styles += `overflow: hidden; `;
+  }
+
+  if ('layoutMode' in node) {
+    const layoutMode = node.layoutMode;
+    if (layoutMode === 'HORIZONTAL') {
+      styles += `flex-direction: row; `;
+    } else if (layoutMode === 'VERTICAL') {
+      styles += `flex-direction: column; `;
+    } else if (layoutMode === 'NONE') {
+      styles += `position: relative; `;
+    }
   }
 
   return styles;
@@ -53,6 +100,7 @@ export function getComputedStyles(node: SceneNode): string {
   }
 
   // TODO: General Styles
+  styles += getGeneralStyles(node);
 
   // TODO: Add more style conversions as needed
 
