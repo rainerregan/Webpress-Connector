1f5df4a1064d40073971b5b7c941c77b88d3930b
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-05-25 10:50:21 +0700

    wip: Use react with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
+  devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
   entry: {
+    ui: './src/ui.tsx', // Entry point for react ui code
     code: './src/code.ts' // This is the entry point for our plugin code.
   },
   module: {
@@ -17,6 +21,18 @@ module.exports = (env, argv) => ({
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /.svg$/,
+        use: '@svgr/webpack',
       },
     ],
   },
@@ -30,5 +46,12 @@ module.exports = (env, argv) => ({
   },
   plugins: [
     new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+      inlineSource: '.(js)$',
+      chunks: ['ui'],
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
   ],
 });
