419bc8992ce63ca7c66faeb31518b103851bd0d5
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-05-25 10:32:22 +0700

    feat: Connect to Webpress (WIP)

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,5 @@
 import { convertRootNodeToHtmlCss } from "./create-html";
-import { login } from "./services/auth.service";
+import { login, registerPlugin } from "./services/auth.service";
 
 figma.showUI(__html__, {
   height: 400,
@@ -7,6 +7,23 @@ figma.showUI(__html__, {
 });
 
 figma.ui.onmessage = async msg => {
+  if (msg.type === 'login') {
+    try {
+      const content = msg.content as { email: string, password: string };
+      if (!content.email || !content.password) throw new Error('Please enter email and password');
+      const loginResponse = await login(content.email, content.password);
+      console.log(loginResponse);
+
+      figma.ui.postMessage({ type: 'login-success', content: loginResponse });
+    } catch (error) {
+      console.log(error);
+
+      const err = error as Error;
+      figma.notify(err.message);
+      return
+    }
+  }
+
   if (msg.type === 'generate-html-css') {
     try {
       const rootNodes = figma.currentPage.selection;
@@ -25,5 +42,26 @@ figma.ui.onmessage = async msg => {
       return
     }
 
+  }
+
+  if (msg.type === 'get-user-projects') {
+    try {
+      const content = msg.content as { email: string, password: string };
+      const figmaUser = figma.currentUser;
+      if (!content.email || !content.password) throw new Error('Please enter email and password');
+      if (!figmaUser) throw new Error('Please login to figma');
+
+      const response = await registerPlugin(content.email, content.password, figmaUser);
+      const result = await response.json();
+      console.log(result);
+
+      figma.ui.postMessage({ type: 'project-updated', content: result });
+    } catch (error) {
+
+      const err = error as Error;
+      console.log(err.message);
+      figma.notify(err.message);
+      return
+    }
   }
 };
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,8 +7,26 @@ export const login = async (email: string, password: string) => {
     method: 'POST',
     body: JSON.stringify({ email, password }),
     headers: {
-      "Access-Control-Allow-Origin": "true"
+      "Access-Control-Allow-Origin": "true",
     }
   })
+  return response;
+}
+
+export const registerPlugin = async (email: string, password: string, figmaUser: User) => {
+  if (!email || !password) throw new Error('Please enter email and password');
+  if (!figmaUser) throw new Error('Please login to figma');
+  const API_URL = process.env.API_URL;
+  const bodyData = JSON.stringify({ email, password, figmaUser });
+
+  const response = await fetch(`${API_URL}/plugin/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: bodyData
+  })
+
   return response;
 }
